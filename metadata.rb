47a81d74a7363780e9abcb696f397abccc2e8404
Damacus <dan.m.webb@gmail.com>
2015-05-14 16:13:20 +0100

    major vrsion bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ai@derby.ac.uk'
 license          'Apache 2.0'
 description      'Installs/Configures fusion-middleware family of products'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1' 
+version          '1.0.0' 
 
 depends 'java'
 depends 'oracle-inventory'
