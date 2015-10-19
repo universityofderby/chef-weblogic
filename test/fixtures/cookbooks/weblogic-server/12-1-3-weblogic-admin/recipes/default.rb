654ec3e82d8fb2dec5c7aa154cabee7c893609d8
Dan Webb <dan.m.webb@gmail.com>
2015-10-19 15:53:45 +0100

    remove erroneous url

--- test/fixtures/cookbooks/weblogic-server/12-1-3-weblogic-admin/recipes/default.rb
+++ test/fixtures/cookbooks/weblogic-server/12-1-3-weblogic-admin/recipes/default.rb
@@ -3,6 +3,3 @@ weblogic '12.1.3' do
   groupname 'weblogic_admin'
   installer_url 'https://www.dropbox.com/s/54blw798dtqgoeo/fmw_12.1.3.0.0_wls.jar'
 end
-
-# node.default['oracle']['inventory']['group'] = 'weblogic_admin'
-# node.default['oracle']['inventory']['user'] = 'weblogic'
