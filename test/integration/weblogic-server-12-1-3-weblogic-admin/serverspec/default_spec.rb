ab95fe38276fb51cb4f435b7badb875776c38390
Dan Webb <dan.m.webb@gmail.com>
2015-10-23 09:56:54 +0100

    update tests to reflect content

--- test/integration/weblogic-server-12-1-3-weblogic-admin/serverspec/default_spec.rb
+++ test/integration/weblogic-server-12-1-3-weblogic-admin/serverspec/default_spec.rb
@@ -2,15 +2,15 @@ require_relative '../../../kitchen/data/spec_helper'
 
 describe user('weblogic') do
   it { should exist }
-  it { belong_to_group 'weblogic-admin' }
+  it { belong_to_group 'weblogic_admin' }
 end
 
-describe group('weblogic-admin') do
+describe group('weblogic_admin') do
   it { should exist }
 end
 
-describe file('/opt/oracle/Middleware/weblogic-12.1.3.0.0/wlserver') do
+describe file('/opt/oracle/Middleware/weblogic-12.1.3/wlserver') do
   it { should be_directory }
   it { should be_owned_by 'weblogic' }
-  it { should be_grouped_into 'weblogic-admin' }
+  it { should be_grouped_into 'weblogic_admin' }
 end
