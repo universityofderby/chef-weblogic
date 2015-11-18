96f246c50d5dd50575b588937e3531ec49102858
Daniel Webb <dan.m.webb@gmail.com>
2015-11-18 15:27:08 +0000

    resource to compat_resource model

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'ai@derby.ac.uk'
 license          'Apache 2.0'
 description      'Installs/Configures fusion-middleware family of products'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.2' 
+version          '2.0.2'
 
 depends 'java'
 depends 'oracle-inventory'
 depends 'chef-sugar'
-depends 'resource'
+depends 'compat_resource'
