f52ebb778bd2880a64b16a15595b9506fe160468
Daniel Webb <dan.m.webb@gmail.com>
2015-11-18 15:27:38 +0000

    Changing defaults to work with new hash syntax

--- resources/weblogic_server.rb
+++ resources/weblogic_server.rb
@@ -18,75 +18,62 @@
 #
 
 property :version, String, name_property: true
-property :cache_path, Path do
-  default { ::File.join(Chef::Config[:file_cache_path], "weblogic-#{version}") }
-end
-property :silent_path, Path do
-  default { ::File.join(cache_path, silent_file) }
-end
-property :silent_file, Path do
-  default do
-    if Gem::Version.new(version) < Gem::Version.new('12.1.2.0.0')
-      'silent.xml'
-    else
-      'silent.rsp'
-    end
+property :cache_path, String, default: ::File.join(Chef::Config[:file_cache_path], "weblogic-#{version}")
+property :silent_path, String, default { ::File.join(cache_path, silent_file) }
+property :silent_file, String, default {
+  if Gem::Version.new(version) < Gem::Version.new('12.1.2.0.0')
+    'silent.xml'
+  else
+    'silent.rsp'
   end
-end
-property :silent_cmd, String do
-  default do
-    if Gem::Version.new(version) < Gem::Version.new('12.1.2.0.0')
-      '-mode=silent -silent_xml='
-    else
-      '-silent -responseFile '
-    end
+}
+property :silent_cmd, String, default{
+  if Gem::Version.new(version) < Gem::Version.new('12.1.2.0.0')
+    '-mode=silent -silent_xml='
+  else
+    '-silent -responseFile '
   end
-end
-property :component_paths, String do
-  default do
-    v = Gem::Version.new(version)
-    v1211 = Gem::Version.new('12.1.1')
-    v1212 = Gem::Version.new('12.1.2')
-    case
-    when v >= v1211 && v < v1212
-      'WebLogic Server/Core Application Server|WebLogic Server/Administration Console|WebLogic Server/Configuration Wizard and Upgrade Framework|WebLogic Server/Web 2.0 HTTP Pub-Sub Server|WebLogic Server/WebLogic JDBC Drivers|WebLogic Server/Third Party JDBC Drivers|WebLogic Server/WebLogic Server Clients|WebLogic Server/Xquery Support|WebLogic Server/Server Examples'
-    else
-      'WebLogic Server/Core Application Server|WebLogic Server/Administration Console|WebLogic Server/Configuration Wizard and Upgrade Framework|WebLogic Server/Web 2.0 HTTP Pub-Sub Server|WebLogic Server/WebLogic JDBC Drivers|WebLogic Server/Third Party JDBC Drivers|WebLogic Server/WebLogic Server Clients|WebLogic Server/WebLogic Web Server Plugins|WebLogic Server/UDDI and Xquery Support|WebLogic Server/Server Examples'
-    end
+}
+
+property :component_paths, String, default{
+  v = Gem::Version.new(version)
+  v1211 = Gem::Version.new('12.1.1')
+  v1212 = Gem::Version.new('12.1.2')
+  case
+  when v >= v1211 && v < v1212
+    'WebLogic Server/Core Application Server|WebLogic Server/Administration Console|WebLogic Server/Configuration Wizard and Upgrade Framework|WebLogic Server/Web 2.0 HTTP Pub-Sub Server|WebLogic Server/WebLogic JDBC Drivers|WebLogic Server/Third Party JDBC Drivers|WebLogic Server/WebLogic Server Clients|WebLogic Server/Xquery Support|WebLogic Server/Server Examples'
+  else
+    'WebLogic Server/Core Application Server|WebLogic Server/Administration Console|WebLogic Server/Configuration Wizard and Upgrade Framework|WebLogic Server/Web 2.0 HTTP Pub-Sub Server|WebLogic Server/WebLogic JDBC Drivers|WebLogic Server/Third Party JDBC Drivers|WebLogic Server/WebLogic Server Clients|WebLogic Server/WebLogic Web Server Plugins|WebLogic Server/UDDI and Xquery Support|WebLogic Server/Server Examples'
   end
-end
-property :installer_url, String do
-  default { node['common_artifact_repo'] + "/oracle/weblogic-server/#{version}/#{installer_file}" }
-end
-property :installer_path, Path do
-  default {  ::File.join(cache_path, installer_file) }
-end
-property :installer_file, Path do
-  default do
-    v = Gem::Version.new(version)
-    v1036 = Gem::Version.new('10.3.6')
-    v1211 = Gem::Version.new('12.1.1')
-    v1212 = Gem::Version.new('12.1.2')
-    v1213 = Gem::Version.new('12.1.3')
-    case
-    when v >= v1036 && v < v1211
-      'wls1036_generic.jar'
-    when v >= v1211 && v < v1212
-      'wls1211_generic.jar'
-    when v >= v1212 && v < v1213
-      'wls_121200.jar'
-    else
-      'fmw_12.1.3.0.0_wls.jar'
-    end
+}
+property :installer_url, String, default {
+  node['common_artifact_repo'] +
+    "/oracle/weblogic-server/#{version}/#{installer_file}"
+}
+property :installer_path, String, default: ::File.join(cache_path, installer_file)
+property :installer_file, String, default {
+  v = Gem::Version.new(version)
+  v1036 = Gem::Version.new('10.3.6')
+  v1211 = Gem::Version.new('12.1.1')
+  v1212 = Gem::Version.new('12.1.2')
+  v1213 = Gem::Version.new('12.1.3')
+  case
+  when v >= v1036 && v < v1211
+    'wls1036_generic.jar'
+  when v >= v1211 && v < v1212
+    'wls1211_generic.jar'
+  when v >= v1212 && v < v1213
+    'wls_121200.jar'
+  else
+    'fmw_12.1.3.0.0_wls.jar'
   end
-end
+}
+
 property :ownername, String, default: 'oracle'
 property :groupname, String, default: 'dba'
-property :home, Path, default: "/opt/oracle/Middleware/weblogic-#{version}"
+property :home, String, default: "/opt/oracle/Middleware/weblogic-#{version}"
 property :oracle_jdk, String, default: '7'
-property :inventory_path, Path do
-  default { ::File.join(home, 'oraInventory') }
-end
+property :inventory_path, String, default: ::File.join(home, 'oraInventory')
 
 action :create do
   node.default['java']['jdk_version'] = oracle_jdk
